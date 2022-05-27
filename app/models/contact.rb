7f9f72a833235e0d4a3835d5113dfb6802f26c54
muellerviana <muellerviana@gmail.com>
2022-05-26 22:16:47 -0300

    aula 16 - kind crud

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,10 @@
 class Contact < ApplicationRecord
+    belongs_to :kind
+    def author
+        "Mueller Viana"
+    end
+
+    def as_json(options={})
+        super(methods: :author, root: true)
+    end
 end
