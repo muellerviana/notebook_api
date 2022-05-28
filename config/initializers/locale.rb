44ea25bc61420984e5fa25a4676d3ad64b54d358
muellerviana <muellerviana@gmail.com>
2022-05-27 22:55:47 -0300

    entendendo o I18n

---
+++ config/initializers/locale.rb
@@ -0,0 +1,9 @@
+#Where th I18 library should serarch for translation files
+
+#I18n.load_path += Dir[Rails.root.join('lib', 'locale', '*.{rb,yml}')]
+
+
+# Set default locale to something other than :en
+
+I18n.default_locale = :'pt-BR'
+
